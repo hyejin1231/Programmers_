54943b4875439d3759ef3cd4f8589b0ef499028c
hyejin <phj981231@naver.com>
2022-10-10 13:07:56 +0900

    20221010 프로그래머스 Lv1. 직사각형 별찍기

--- src/level1/Descending.java
+++ src/level1/Descending.java
@@ -1,38 +1,25 @@
 package level1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
-/**
- * 프로그래머스 Lv1.
- * 문자열 내림차순으로 배치하기
- * 대문자는 소문자보다 작은 것으로 인식
- */
 public class Descending {
+
     public static void main(String[] args) {
         Descending descending = new Descending();
-        String result = descending.otherSolution("Zbcdefg");
-        System.out.println("result = " + result); // result = gfedcbZ
-
+        long result = descending.MySolution(118372);
+        System.out.println("result = " + result); // result = 873211
     }
 
+    public long MySolution(long n) {
+        long answer = 0;
 
-    public String solution(String s) {
-        String[] split = s.split("");
-        Arrays.sort(split, Collections.reverseOrder());
 
-        StringBuilder sb = new StringBuilder();
-        for (String s1 : split) {
-            sb.append(s1);
-        }
-
-        return sb.toString();
+        return answer;
     }
 
-    public String otherSolution(String s) {
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-
-        return new StringBuilder(new String(chars)).reverse().toString();
-    }
 }
