d2618147c95a80d50ec791b35e44cbd89e0ce6e1
hyejin <phj981231@naver.com>
2023-01-04 15:32:24 +0900

    20230104 프로그래머스 Lv1. 제일 작은 수 제거하기

--- src/level1/SmallestNumber.java
+++ src/level1/SmallestNumber.java
@@ -1,8 +1,6 @@
 package level1;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * 프로그래머스 Lv1.
@@ -11,15 +9,32 @@ import java.util.Comparator;
 public class SmallestNumber {
     public static void main(String[] args) {
         SmallestNumber smallestNumber = new SmallestNumber();
-        int[] arr = {4, 3, 2, 1};
+//        int[] arr = {4, 3, 2, 1};
+        int[] arr = {-10};
         int[] result = smallestNumber.solution(arr);
         System.out.println("Arrays.toString(result) = " + Arrays.toString(result)); // result = 4, 3, 2
     }
 
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        if (arr.length == 0 || ( arr.length == 1&& arr[0] == 10)) {
+            return new int[]{-1};
+        }
 
+        int[] result = new int[arr.length - 1];
+        int min = arr[0];
 
-        return answer;
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min) {
+                continue;
+            }
+            result[index++] = arr[i];
+        }
+        return result;
+
     }
 }
