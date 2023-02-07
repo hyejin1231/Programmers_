5c517b777f9d5ca870189b85ad7fde0f692a78c4
hyejin1231 <phj981231@naver.com>
2023-02-07 14:56:05 +0900

    20230207 프로그래머스 Lv1. 가장 가까운 같은 글자 - 다른 풀이 추가

--- src/level1/NearestNumber.java
+++ src/level1/NearestNumber.java
@@ -38,4 +38,19 @@ public class NearestNumber
 		}
 		return answer;
 	}
+	
+	public int[] otherSolution(String s)
+	{
+		int[] answer = new int[s.length()];
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (int i =0; i< s.length(); i++)
+		{
+			char c = s.charAt(i);
+			answer[i] = i - map.getOrDefault(c, i + 1);
+			map.put(c, i);
+		}
+		
+		return answer;
+	}
 }
